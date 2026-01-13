fd28bb45bba0bd9252e19270ab11522447a6b928
safwan7711 <safwan100408@gmail.com>
2026-01-13 21:00:33 +0530

    spring_system_cp1

---
+++ build/spring_system/CMakeFiles/spring_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spring_system.dir/src/spring_system.cpp.o"
+  "CMakeFiles/spring_system.dir/src/spring_system.cpp.o.d"
+  "spring_system"
+  "spring_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spring_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
