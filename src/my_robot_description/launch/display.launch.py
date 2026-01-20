9e70c679c34056b8ac1364d4c6a07042a6987124
Rishabh Meel <meelrishabh4761@gmail.com>
2026-01-20 16:20:11 +0530

    Finally able to see the robot in rviz

---
+++ src/my_robot_description/launch/display.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+import xacro
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('my_robot_description')
+    xacro_path = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
+    rviz_config = os.path.join(pkg_path,'rviz','display.rviz')
+
+    robot_description_config = xacro.process_file(xacro_path)
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{
+                'robot_description': robot_description_config.toxml()
+            }]
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', rviz_config ],
+            output='screen'
+        )
+    ])
